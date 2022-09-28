1ac618ad0128d8be8768af92d78fd09c809d9b63
erumaro <me@tobiasarud.se>
2022-09-28 17:59:47 +0200

    Beginning of Skills section.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import 'normalize.css';
 import './App.css';
 import logo from '../public/images/logo.png';
-import hero from '../public/images/hero.jpg';
+import herooriginal from '../public/images/hero.jpg';
+import herosmall from '../public/images/hero375.jpg';
+import heromedium from '../public/images/hero768.jpg';
+import herolarge from '../public/images/hero1080.jpg';
 import portraitoriginal from '../public/images/tobias.jpg';
 import portraitsmall from '../public/images/tobias375.jpg';
 import portraitmedium from '../public/images/tobias768.jpg';
@@ -16,7 +19,7 @@ class App extends React.Component {
                     <a className="app-logo" href="https://www.eruma.se"><img className="logo" src={logo} alt="Eruma" /></a>
                 </header>
                 <main className="app-main content">
-                    <section id="sectionIntro" className="section section-intro" style={{ backgroundImage: `url(${hero})` }}>
+                    <section id="sectionIntro" className="section section-intro" style={{ backgroundImage: `url(${herooriginal})` }}>
                         <div>
                             <h2>Hi, I'm Tobias!</h2>
                             <p>
@@ -37,12 +40,66 @@ class App extends React.Component {
                             <img className="portrait" 
                                 src={portraitsmall} 
                                 srcSet={`${portraitsmall} 375w, ${portraitmedium} 768w, ${portraitlarge} 1080w, ${portraitoriginal} 1980w`} 
-                                sizes="(max-width: 400px) 100vw, (max-width: 700px) 50vw, (max-width: 900px) 33vw, 225px"
+                                sizes="(max-width: 400px) 100vw"
                                 alt="Tobias" />
                         </div>
                     </section>
                     <section id="sectionSkills" className="section section-skills">
-                        <h2>This is for listing skills and tools.</h2>
+                        <h2>Skills and Tools</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim sodales augue, et dapibus est faucibus ut. 
+                            Nam fermentum ut ante vitae tristique. Etiam malesuada libero euismod vehicula egestas. Nullam euismod faucibus est a vehicula. 
+                            Mauris in augue ut metus laoreet pretium vitae et leo.
+                        </p>
+                        <div className="columns">
+                            <div className="column">
+                                <div className="skill-card">
+                                    <h3>Front-end</h3>
+                                    <ul>
+                                        <li>HTML5</li>
+                                        <li>CSS3</li>
+                                        <li>JavaScript
+                                            <ul>
+                                                <li>Vanilla</li>
+                                                <li>jQuery</li>
+                                                <li>ES2016+</li>
+                                                <li>React</li>
+                                                <li>Vue</li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                    <ul>
+                                        <li>Webpack</li>
+                                        <li>Gulp</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="column">
+                                <div className="skill-card">
+                                    <h3>Back-end</h3>
+                                    <ul>
+                                        <li>php
+                                            <ul>
+                                                <li>Wordpress</li>
+                                                <li>Drupal</li>
+                                                <li>Symfony2 Twig</li>
+                                            </ul>
+                                        </li>
+                                        <li>MySQL</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="column">
+                                <div className="skill-card">
+                                    <h3>?</h3>
+                                    <ul>
+                                        <li>Qué</li>
+                                        <li>What</li>
+                                        <li>Vad</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </section>
                     <section id="sectionXp" className="section section-xp">
                         <h2>This is for portfolio and resumé content.</h2>
