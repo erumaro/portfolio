e2f7cf56e74e1fedd19b63c4926df54fd0904c2b
erumaro <me@tobiasarud.se>
2022-09-30 17:01:23 +0200

    Continued styling. Brainstorm the unfinished sections on Monday!

--- src/App.jsx
+++ src/App.jsx
@@ -52,85 +52,89 @@ class App extends React.Component {
                             <p><a href="https://github.com/erumaro">Check my repos at Erumaro on Github <FontAwesomeIcon icon={faGithubAlt} /></a> or <a href="#contact">contact me</a> to here more!</p>
                         </div>
                     </div>
-                    <section id="sectionAbout" className="app-about">
-                        <div className="flex">
-                            <h2>About me</h2>
-                            <div className="text-bubble">
-                                <p>I'm coming from a background of Interaction design and front-end development and work prominently with Wordpress both in back-end and front-end.</p>
+                    <section id="sectionAbout" className="app-about container">
+                        <div className="grid">
+                            <div className="column">
+                                <h2>About me</h2>
+                                <img className="portrait" src={portraitsmall} alt="Tobias" />
                             </div>
-                            <img src={portraitsmall} alt="Tobias" />
-                            <div className="text-bubble">
+                            <div className="column">
+                                <p>I'm coming from a background of Interaction design and front-end development and work prominently with Wordpress both in back-end and front-end.</p>
                                 <p>Besides great knowledge in Wordpress development I'm also knowledgable in accessibility and strive to build my websites with the users 
-                                    experience in mind.</p>
+                                experience in mind.</p>
                             </div>
                         </div>
                     </section>
                     <section id="sectionSkills">
-                        <h2>Fullstack skills with Front-end focus</h2>
-                        <p>As a developer I like exploring new languages, design patterns and tools to become even better at what I know.</p>
-                        <div className="skills">
-                            <div>
-                                <button id="frontend" className="filter-button">
-                                    Front-end
-                                </button>
-                                <button id="backend" className="filter-button">
-                                    Back-end
-                                </button>
-                                <button id="fullstack" className="filter-button">
-                                    Full stack
-                                </button>
-                                <button id="design" className="filter-button">
-                                    Design
-                                </button>
+                        <div className="container">
+                            <h2>Fullstack skills with Front-end focus</h2>
+                            <p>As a developer I like exploring new languages, design patterns and tools to become even better at what I know.</p>
+                            <div className="skills">
+                                <div>
+                                    <button id="frontend" className="filter-button">
+                                        Front-end
+                                    </button>
+                                    <button id="backend" className="filter-button">
+                                        Back-end
+                                    </button>
+                                    <button id="fullstack" className="filter-button">
+                                        Full stack
+                                    </button>
+                                    <button id="design" className="filter-button">
+                                        Design
+                                    </button>
+                                </div>
+                                <ul>
+                                    <li>HTML5</li>
+                                    <li>CSS</li>
+                                    <li>JavaScript</li>
+                                    <li>React (JS Library)</li>
+                                    <li>jQuery (JS Library)</li>
+                                    <li>SCSS</li>
+                                    <li>Composer</li>
+                                    <li>Node.js</li>
+                                    <li>Wordpress</li>
+                                    <li>MySQL</li>
+                                    <li>PHP</li>
+                                </ul>
+                                <p>For more info on each skill or tool checkout my Wordpress site at <a href="https://eruma.se">Eruma.se</a></p>
                             </div>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>React (JS Library)</li>
-                                <li>jQuery (JS Library)</li>
-                                <li>SCSS</li>
-                                <li>Composer</li>
-                                <li>Node.js</li>
-                                <li>Wordpress</li>
-                                <li>MySQL</li>
-                                <li>PHP</li>
-                            </ul>
-                            <p>For more info on each skill or tool checkout my Wordpress site at <a href="https://eruma.se">Eruma.se</a></p>
                         </div>
                     </section>
                     <section id="sectionXp">
-                        <h2>I gained my experience at several great places</h2>
-                        <p>Every place I have been at have been helping grow as developer and colleague and I hope you will be my next step!</p>
-                        <div className="columns">
-                            <div className="column">
-                                <div className="xp-card">
-                                    <h3>Company/School</h3>
-                                    <ul>
-                                        <li>Made coffee</li>
-                                        <li>Commuted</li>
-                                        <li>Slept well/badly</li>
-                                    </ul>
+                        <div className="container">
+                            <h2>I gained my experience at several great places</h2>
+                            <p>Every place I have been at have been helping grow as developer and colleague and I hope you will be my next step!</p>
+                            <div className="grid">
+                                <div className="column">
+                                    <div className="xp-card">
+                                        <h3>Company/School</h3>
+                                        <ul>
+                                            <li>Made coffee</li>
+                                            <li>Commuted</li>
+                                            <li>Slept well/badly</li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="column">
-                                <div className="xp-card">
-                                    <h3>Company/School</h3>
-                                    <ul>
-                                        <li>Made coffee</li>
-                                        <li>Commuted</li>
-                                        <li>Slept well/badly</li>
-                                    </ul>
+                                <div className="column">
+                                    <div className="xp-card">
+                                        <h3>Company/School</h3>
+                                        <ul>
+                                            <li>Made coffee</li>
+                                            <li>Commuted</li>
+                                            <li>Slept well/badly</li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="column">
-                                <div className="xp-card">
-                                    <h3>Company/School</h3>
-                                    <ul>
-                                        <li>Made coffee</li>
-                                        <li>Commuted</li>
-                                        <li>Slept well/badly</li>
-                                    </ul>
+                                <div className="column">
+                                    <div className="xp-card">
+                                        <h3>Company/School</h3>
+                                        <ul>
+                                            <li>Made coffee</li>
+                                            <li>Commuted</li>
+                                            <li>Slept well/badly</li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
