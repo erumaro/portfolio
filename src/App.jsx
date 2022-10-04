9245bb4c011a4c162e88fd73ad01c76aa6d05dd8
erumaro <me@tobiasarud.se>
2022-10-04 17:13:00 +0200

    Focused on making the about section better. Also an experimental change to intro h1. Next up skills and xp!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import 'normalize.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlasses, faDisplay, faTimeline, faAddressCard } from '@fortawesome/free-solid-svg-icons'
+import { faGlasses, faDisplay, faTimeline, faAddressCard, faPencil, faBookJournalWhills, faHeadphones } from '@fortawesome/free-solid-svg-icons'
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 import './App.scss';
 import logo from '../public/images/logo.png';
 import herolarge from '../public/images/hero1080.jpg';
+import tobiastext from '../public/images/tobias.png';
 import portraitoriginal from '../public/images/tobias.jpg';
 import portraitsmall from '../public/images/tobias375.jpg';
 import portraitmedium from '../public/images/tobias768.jpg';
@@ -47,21 +48,29 @@ class App extends React.Component {
                 <main className="app-main content">
                     <div id="hero" className="app-intro" style={{ backgroundImage: `url(${herolarge})` }}>
                         <div className="flex">
-                            <h1>Hi I'm Tobias!</h1>
+                            <h1>Hi I'm <img className="tobiastext" src={tobiastext} alt="Tobias" />!</h1>
                             <p>I handle code from both back-end and front-end.</p>
                             <p><a href="https://github.com/erumaro">Check my repos at Erumaro on Github <FontAwesomeIcon icon={faGithubAlt} /></a> or <a href="#contact">contact me</a> to here more!</p>
                         </div>
                     </div>
-                    <section id="sectionAbout" className="app-about container">
-                        <div className="grid">
-                            <div className="column">
-                                <h2>About me</h2>
-                                <img className="portrait" src={portraitsmall} alt="Tobias" />
-                            </div>
-                            <div className="column">
-                                <p>I'm coming from a background of Interaction design and front-end development and work prominently with Wordpress both in back-end and front-end.</p>
-                                <p>Besides great knowledge in Wordpress development I'm also knowledgable in accessibility and strive to build my websites with the users 
-                                experience in mind.</p>
+                    <section id="sectionAbout" className="app-about">
+                        <div className="container">
+                            <div className="grid">
+                                <div className="column">
+                                    <h2>About me</h2>
+                                    <p>I'm coming from a background of Interaction design and front-end development and work prominently with Wordpress both in back-end and front-end.</p>
+                                    <p>Besides great knowledge in Wordpress development I'm also knowledgable in accessibility and strive to build my websites with the users 
+                                    experience in mind.</p>
+                                    <h3>Short facts</h3>
+                                    <ul>
+                                        <li><FontAwesomeIcon icon={faHeadphones} /> Eclective music taste, ranging from pop to Melodic Death Metal.</li>
+                                        <li><FontAwesomeIcon icon={faBookJournalWhills} /> Nerdy interest in Fantasy literature and Sci-fi like Star Wars</li>
+                                        <li><FontAwesomeIcon icon={faPencil} /> Like improving my drawing skills as a hobby.</li>
+                                    </ul>
+                                </div>
+                                <div className="column">
+                                    <img className="portrait" src={portraitsmall} alt="Tobias" />
+                                </div>
                             </div>
                         </div>
                     </section>
